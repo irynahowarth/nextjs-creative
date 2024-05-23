2fe75d582a70ec474bb85509804534136b37f38b
Iryna Howarth <irynahowarth@gmail.com>
2024-05-23 14:10:21 +0100

    [feature]: add Header component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { PrismicPreview } from "@prismicio/next";
 import {Urbanist} from 'next/font/google'
 import { repositoryName } from "@/prismicio";
 import './globals.css'
+import Header from "@/components/Header";
 
 const urbanist = Urbanist({subsets:['latin']})
 
@@ -12,8 +13,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="bg-slate-900 text-slate-100">
-      <body className={urbanist.className}>{children}</body>
-      <PrismicPreview repositoryName={repositoryName} />
+      <body className={urbanist.className}>
+        <Header/>
+        {children}
+      </body>
+        <PrismicPreview repositoryName={repositoryName} />
     </html>
   );
 }
