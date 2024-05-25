e0d0ba0c901b7fc90bf1d1db51114de77407ea9b
Iryna Howarth <irynahowarth@gmail.com>
2024-05-25 13:04:59 +0100

    [feature]: add movement and highlight (w gsap) to Avatar in Biography slice

--- src/slices/Biography/Avatar.tsx
+++ src/slices/Biography/Avatar.tsx
@@ -20,7 +20,30 @@ export default function Avatar({ image, className }: AvatarProps) {
         { opacity: 0, scale: 1.4 },
         {scale:1, opacity:1, duration: 1.3, ease: "power3.inOut"}
       )
-    })
+
+      window.onmousemove = (e) => {
+        if (!component.current) return;
+        const componentRect = (component.current as HTMLElement).getBoundingClientRect()
+        const componentCenterX = componentRect.left + componentRect.width / 2
+        
+        let componentPercent = {
+          x: (e.clientX-componentCenterX)/componentRect.width / 2
+        }
+        let distFromCenter = 1 - Math.abs(componentPercent.x)
+
+        gsap.timeline({
+          defaults: { duration: 0.5, overwrite: 'auto', ease: 'power3.out'}
+        }).to('.avatar', {
+          rotation: gsap.utils.clamp(-2, 2, 5 * componentPercent.x),
+          duration: 0.5
+        }, 0
+        ).to('.highlight', {
+          opacity: distFromCenter - 0.7,
+          x: -10 + 20 * componentPercent.x,
+          duration:.5
+        },0)
+      }
+    }, component)
   },[])
   return (
     <div
@@ -33,7 +56,7 @@ export default function Avatar({ image, className }: AvatarProps) {
           className="avatar-image h-full w-full object-fill "
           imgixParams={{q:90}}
         />
-        <div className="highligh absolute inset-0 hidden w-full scale-110 bg-gradient-to-tr from-transparent via-white to-transparent opacity-0 md:block"></div>
+        <div className="highlight absolute inset-0 hidden w-full scale-110 bg-gradient-to-tr from-transparent via-white to-transparent opacity-0 md:block"></div>
       </div>
     </div>
   )
