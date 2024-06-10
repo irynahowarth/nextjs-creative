5283e54c186a643bf6348eb4517310167b01fdcf
Iryna Howarth <irynahowarth@gmail.com>
2024-06-10 14:09:58 +0100

    [feature]: add single Blog and single Project. add ImageBlock and TextBlock slices

---
+++ src/slices/ImageBlock/index.tsx
@@ -0,0 +1,28 @@
+import { Content } from "@prismicio/client";
+import { PrismicNextImage } from "@prismicio/next";
+import { SliceComponentProps } from "@prismicio/react";
+
+/**
+ * Props for `ImageBlock`.
+ */
+export type ImageBlockProps = SliceComponentProps<Content.ImageBlockSlice>;
+
+/**
+ * Component for "ImageBlock" Slices.
+ */
+const ImageBlock = ({ slice }: ImageBlockProps): JSX.Element => {
+  return (
+    <section
+      data-slice-type={slice.slice_type}
+      data-slice-variation={slice.variation}
+    >
+      <PrismicNextImage field={slice.primary.image}
+        imgixParams={
+         {w:600}
+        }
+      />
+    </section>
+  );
+};
+
+export default ImageBlock;
